30502eec0aa476c2f6faf03271cf4b97be3f75f9
Stefano Simonelli <16114781+s-simoncelli@users.noreply.github.com>
2024-04-07 06:42:59 +0100

    Use workspace to organise code

--- gr6j-core/examples/two_hydrological_unit_model.rs
+++ gr6j-core/examples/two_hydrological_unit_model.rs
@@ -8,12 +8,14 @@ use log::LevelFilter;
 use std::fs::File;
 use std::path::Path;
 
+/// Use two hydrological units or sub-catchments to model two different responses from the same
+/// catchment.
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Enable logging
     env_logger::builder().filter_level(LevelFilter::Info).init();
 
     // Collect hydrological data
-    let file = File::open(r"src\test_data\airGR_L0123001_dataset.csv")?;
+    let file = File::open(r"gr6j-core\src\test_data\airGR_L0123001_dataset.csv")?;
     let mut rdr = csv::Reader::from_reader(file);
 
     let mut time: Vec<NaiveDate> = vec![];
@@ -32,26 +34,38 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     // Configure the model
-    let start = NaiveDate::from_ymd_opt(1994, 1, 1).unwrap();
+    let start = NaiveDate::from_ymd_opt(1984, 1, 1).unwrap();
     let end = NaiveDate::from_ymd_opt(1998, 12, 31).unwrap();
 
     let inputs = GR6JModelInputs {
         time,
         precipitation: precipitation.clone(),
         evapotranspiration: evapotranspiration.clone(),
-        catchment: CatchmentType::OneCatchment(CatchmentData {
-            area: 1.0,
-            x1: Parameter::X1(31.0),
-            x2: Parameter::X2(3.47),
-            x3: Parameter::X3(32.0),
-            x4: Parameter::X4(2.1),
-            x5: Parameter::X5(0.55),
-            x6: Parameter::X6(5.3),
-            store_levels: None,
-        }),
+        catchment: CatchmentType::SubCatchments(vec![
+            CatchmentData {
+                area: 2.0,
+                x1: Parameter::X1(31.0),
+                x2: Parameter::X2(3.47),
+                x3: Parameter::X3(32.0),
+                x4: Parameter::X4(2.1),
+                x5: Parameter::X5(0.55),
+                x6: Parameter::X6(5.3),
+                store_levels: None,
+            },
+            CatchmentData {
+                area: 0.4,
+                x1: Parameter::X1(1000.0),
+                x2: Parameter::X2(1.00),
+                x3: Parameter::X3(3.0),
+                x4: Parameter::X4(1.2),
+                x5: Parameter::X5(3.0),
+                x6: Parameter::X6(1.3),
+                store_levels: None,
+            },
+        ]),
         run_period: ModelPeriod { start, end },
         warmup_period: None,
-        destination: Some(Path::new(r"./examples/results").to_path_buf()),
+        destination: Some(Path::new(r"gr6j-core\examples\results").to_path_buf()),
         observed_runoff: Some(observed),
         run_off_unit: RunOffUnit::NoConversion,
     };
